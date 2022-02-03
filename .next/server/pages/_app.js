4cfde6be7ec2de139181bf15be0922a15b080e55
jmorettoni <jmorettoni@gmail.com>
2022-02-02 21:37:54 -0300

    change gallery to home

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -5,24 +5,58 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 510:
+/***/ 525:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "styled-components"
+var external_styled_components_ = __webpack_require__(518);
+;// CONCATENATED MODULE: ./src/styles/global.js
+
+const GlobalStyle = external_styled_components_.createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Russo One', sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    cursor: pointer;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+;// CONCATENATED MODULE: ./src/pages/_app.js
 
 
 
 function MyApp({ Component , pageProps  }) {
-    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
-        ...pageProps
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(GlobalStyle, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                ...pageProps
+            })
+        ]
     }));
 }
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
+/* harmony default export */ const _app = (MyApp);
 
 
 /***/ }),
@@ -31,6 +65,13 @@ function MyApp({ Component , pageProps  }) {
 /***/ ((module) => {
 
 module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 518:
+/***/ ((module) => {
+
+module.exports = require("styled-components");
 
 /***/ })
 
@@ -41,7 +82,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(510));
+var __webpack_exports__ = (__webpack_exec__(525));
 module.exports = __webpack_exports__;
 
 })();
--- .next/server/pages/api/hello.js
+++ .next/server/pages/api/hello.js
@@ -5,7 +5,7 @@ exports.id = 453;
 exports.ids = [453];
 exports.modules = {
 
-/***/ 677:
+/***/ 665:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
@@ -29,7 +29,7 @@ function handler(req, res) {
 var __webpack_require__ = require("../../webpack-api-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(677));
+var __webpack_exports__ = (__webpack_exec__(665));
 module.exports = __webpack_exports__;
 
 })();
